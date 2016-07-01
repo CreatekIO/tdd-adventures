daf425a7e32a34b8b4ccebce8ec7e6b15145b3e4
John Cleary <john@createk.io>
2016-07-01 22:12:52 +0100

    first clue

---
+++ lib/tasks/adventures.rb
@@ -0,0 +1,40 @@
+desc 'Get the next clue'
+task :clue => 'adventures:clue'
+
+namespace :adventures do
+
+  task :clue => :header do
+    puts " The evil  Lord Waterfall,  bent on  destroying  all XP  practices has"
+    puts " deleted  all your  code. Luckily  for you  he didn't  see the  'spec'"
+    puts " folder, so not all is lost! "
+    puts
+    puts " Your  mission is to  recover your  code using only  the tests  as your"
+    puts " guide.  Once  you  fix  the  first  test  you will  be  given  further"
+    puts " instructions."
+    puts
+    puts " Good luck!"
+    puts
+    puts " To continue, run:"
+    puts "   rspec"
+    puts
+  end
+
+
+  task :header do
+    puts
+    puts '    __________  ____ '
+    puts '   /_  __/ __ \/ __ \ '
+    puts '    / / / / / / / / / '
+    puts '   / / / /_/ / /_/ / '
+    puts '  /_/ /_____/_____/ '
+    puts '              ___       __                 __ '
+    puts '             /   | ____/ /   _____  ____  / /___  __________  _____ '
+    puts '            / /| |/ __  / | / / _ \/ __ \/ __/ / / / ___/ _ \/ ___/ '
+    puts '           / ___ / /_/ /| |/ /  __/ / / / /_/ /_/ / /  /  __(__  ) '
+    puts '          /_/  |_\__,_/ |___/\___/_/ /_/\__/\__,_/_/   \___/____/ '
+    puts
+    puts ' ---------------------------------------------------------------------'
+  end
+end
+
+
